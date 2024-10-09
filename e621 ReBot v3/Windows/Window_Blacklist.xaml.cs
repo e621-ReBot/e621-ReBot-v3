0a3a69e5e65ed6a5814c133c5c8fba4fa16b3370
Anonymous <Anonymous>
2024-10-09 02:09:50 +0200

    v3.0.0.42 - Fix blacklist not accepting newline.

--- e621 ReBot v3/Windows/Window_Blacklist.xaml.cs
+++ e621 ReBot v3/Windows/Window_Blacklist.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace e621_ReBot_v3
 {
@@ -20,16 +21,36 @@ namespace e621_ReBot_v3
         {
             if (AppSettings.Blacklist.Any())
             {
-                Blacklist_TextBox.Text = string.Join(' ', AppSettings.Blacklist);
+                Blacklist_TextBox.Text = string.Join(Environment.NewLine, AppSettings.Blacklist) + Environment.NewLine;
                 Blacklist_TextBox.SelectionStart = Blacklist_TextBox.Text.Length;
             }
             Blacklist_TextBox.Focus();
         }
 
+        private void Blacklist_TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    {
+                        if (Blacklist_TextBox.SelectionStart > 2)
+                        {
+                            string NewLineChecker = Blacklist_TextBox.Text.Substring(Blacklist_TextBox.SelectionStart - 2, 2);
+                            if (NewLineChecker.Equals("\r\n"))
+                            {
+                                e.Handled = true;
+                                return;
+                            }
+                        }
+                        break;
+                    }
+            }
+        }
+
         private void Window_Closed(object sender, EventArgs e)
         {
             List<string> NewBlacklist = new List<string>();
-            Blacklist_TextBox.Text = Blacklist_TextBox.Text.ToLower();
+            Blacklist_TextBox.Text = Blacklist_TextBox.Text.Trim().ToLower();
             for (int i = 0; i < Blacklist_TextBox.LineCount; i++)
             {
                 string TBLine = Blacklist_TextBox.GetLineText(i).Trim();
